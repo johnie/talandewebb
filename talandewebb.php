31052f72e22d39e1a6a60dbaf897351de342d779
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-12 09:10:02 +0100

    Initial commit

---
+++ talandewebb.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* Plugin Name: Talande Webb
+* Plugin URI: https://github.com/johnie/talandewebb
+* Description: WordPress plugin for FunkaNU screen reader application Talande Webb
+* Version: 1.0.0
+* Author: Johnie Hjelm
+* Author URI: http://johnie.se
+* License: GPL2
+*/
+
+/*  
+    Copyright 2015 Johnie Hjelm <johniehjelm@me.com> (http://johnie.se)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+class TalandeWebb {
+
+  function __construct() {
+
+    // Don't load Talande Webb in admin
+    if ( ! is_admin() ):
+      add_action( 'wp_enqueue_scripts', array( $this, 'register_tw_script' ) );
+    endif;
+
+  }
+
+  public function register_tw_script() {
+    wp_enqueue_script( 'talandewebb-script', plugins_url( 'talandewebb/talandewebb.js' ) );
+  }
+
+}
+
+$talandewebb = new TalandeWebb();
