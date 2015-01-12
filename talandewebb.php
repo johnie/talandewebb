c11d48dbee37acc0f7d3b238ded0ab8a87d0a494
Fredrik Forsmo <frozzare@users.noreply.github.com>
2015-01-12 16:43:07 +0100

    Changed to option page

--- talandewebb.php
+++ talandewebb.php
@@ -58,7 +58,7 @@ if ( ! class_exists( 'TalandeWebb' ) ) {
      * @access public
      */
     function _tw_settings_menu() {
-      add_menu_page( __( 'Talande Webb Plus', 'talandewebb' ), __( 'Talande Webb Plus', 'talandewebb' ), 'manage_options', 'tw-plugin-options', array( $this, '_tw_render_plugin_options' ) );
+      add_options_page( __( 'Talande Webb Plus', 'talandewebb' ), __( 'Talande Webb Plus', 'talandewebb' ), 'manage_options', 'tw-plugin-options', array( $this, '_tw_render_plugin_options' ) );
     }
 
 
